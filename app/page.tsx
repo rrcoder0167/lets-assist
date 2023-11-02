39401c8913ac52389d77ed1904125aa7384d7f34
rrcoder0167 <riddhiman.rana@gmail.com>
2023-11-02 16:31:55 -0700

    finally fixing bootstrap after 36 hours of pain

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,45 @@
-import Image from 'next/image'
+
+import React from 'react';
 
 export default function Home() {
   return (
-    <div>
-      <header>
-        <nav className="navbar">
-          <div className="brand">Let's Assist</div>
-          <ul className="nav-links">
-            <li><a href="/login">Login</a></li>
-            <li><a href="/signup">Signup</a></li>
-            <li><a href="/learnmore">Learn More</a></li>
+    <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Link</a>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul className="dropdown-menu">
+            <li><a className="dropdown-item" href="#">Action</a></li>
+            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li><hr className="dropdown-divider"></hr></li>
+            <li><a className="dropdown-item" href="#">Something else here</a></li>
           </ul>
-        </nav>
-      </header>
-      <main>
-        {/* Your homepage content goes here */}
-      </main>
-      {/* Add footer or other content here */}
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
     </div>
-  )
+  </div>
+</nav>
+    </>
+  );
 }
