1978052e152502ba10fb14945d5539356ee6f3c4
rrcoder0167 <riddhiman.rana@gmail.com>
2023-11-02 23:11:57 -0700

    add geist font support, and add get started button

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './globals.css'
-import React from 'react'
-const inter = Inter({ subsets: ['latin'] })
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './globals.css';
+import React from 'react';
+import { GeistSans, GeistMono } from 'geist/font';
+const inter = Inter({ subsets: ['latin'] });
 import ImportBsJS from "@/components/importBsJs";
 
 export const metadata: Metadata = {
@@ -13,10 +14,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({children,}: {children: React.ReactNode;}) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${GeistSans.variable}`}>
       <body>
         <ImportBsJS />
-        <div>{children}</div>
+        {children}
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './page.css'
-import { GeistSans, GeistMono } from 'geist/font'
 
 export default function Home() {
   return (
@@ -34,6 +33,7 @@ export default function Home() {
       <main className="d-flex align-items-center justify-content-center flex-column" style={{height: '80vh'}}>
         <h1 className="landing-text">Give back to your<br/>community, your way</h1>
         <p className="sub-text">Find local volunteering opportunities <span className="highlight">today</span> with Let's Assist, and give back to your community</p>
+        <button className="btn btn-success mt-3">Get Started</button>
       </main>
     </>
   );
