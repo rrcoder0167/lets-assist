df7f1de170cc7f7d9286caea4ab720dfe4f799f5
rrcoder0167 <riddhiman.rana@gmail.com>
2023-11-02 17:39:50 -0700

    sorting out js issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,24 +5,21 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 const inter = Inter({ subsets: ['latin'] })
 import "bootstrap/dist/css/bootstrap.min.css";
-import ImportBsJS from "@/components/importBsJs";
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: "Let's Assist",
   description: 'Volunteering for everyone',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout ({ children }) {
   return (
     <html lang="en">
       <body>
-        <ImportBsJS />
-        <div>{children}</div>
+        {children}
+        <Script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></Script>
+        <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></Script>
       </body>
     </html>
   );
-}
+};
