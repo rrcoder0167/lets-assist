b382d4b97ac3d44866adb99e7fed320d72625c39
Riddhiman Rana <riddhiman.rana@gmail.com>
2023-11-03 17:26:41 +0000

    Update 10 files

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 import './page.css'
 
 export default function Home() {
@@ -6,8 +7,8 @@ export default function Home() {
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-light px-3">
         <a className="navbar-brand font-weight-bold" href="#">
-          <img src="path_to_your_logo.png" width="30" height="30" alt="Let's Assist"/>
-          Let's Assist
+          <Image src="/letsassist-logo.svg" width="30" height="30" alt="Let's Assist"/>
+          Let&apos;s Assist
         </a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
@@ -32,7 +33,7 @@ export default function Home() {
       </nav>
       <main className="d-flex align-items-center justify-content-center flex-column" style={{height: '80vh'}}>
         <h1 className="landing-text">Give back to your<br/>community, your way</h1>
-        <p className="sub-text">Find local volunteering opportunities <span className="highlight">today</span> with Let's Assist, and give back to your community</p>
+        <p className="sub-text">Find local volunteering opportunities <span className="highlight">today</span> with Let&apos;s Assist, and give back to your community</p>
         <button className="btn btn-success mt-3">Get Started</button>
       </main>
     </>
