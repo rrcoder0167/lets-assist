dd8425a9352ec2e9a4971b02f5e2e25fa04b8a10
rrcoder0167 <riddhiman.rana@gmail.com>
2023-11-04 09:57:55 -0700

    Add new sections and update navbar styles

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,46 @@ export default function Home() {
         <p className="sub-text">Find local volunteering opportunities <span className="highlight">today</span> with Let&apos;s Assist, and give back to your community</p>
         <button className="btn btn-success mt-3">Get Started</button>
       </main>
+      <ProjectMatchmaking />
+      <RealTimeProjectUpdates />
+      <VolunteerRecognition />
+      <CommunityForums />
     </>
+  );
+}
+
+function ProjectMatchmaking() {
+  return (
+    <section>
+      <h2>Project Matchmaking</h2>
+      <p>Description for Project Matchmaking...</p>
+    </section>
+  );
+}
+
+function RealTimeProjectUpdates() {
+  return (
+    <section>
+      <h2>Real-Time Project Updates</h2>
+      <p>Description for Real-Time Project Updates...</p>
+    </section>
+  );
+}
+
+function VolunteerRecognition() {
+  return (
+    <section>
+      <h2>Volunteer Recognition</h2>
+      <p>Description for Volunteer Recognition...</p>
+    </section>
+  );
+}
+
+function CommunityForums() {
+  return (
+    <section>
+      <h2>Community Forums</h2>
+      <p>Description for Community Forums...</p>
+    </section>
   );
 }
