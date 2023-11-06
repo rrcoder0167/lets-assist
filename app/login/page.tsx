768397b9dd160e59583b1b00850e0fe8f348db7f
rrcoder0167 <riddhiman.rana@gmail.com>
2023-11-05 17:45:48 -0800

    Update login and signup pages with OAuth buttons and signup link

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,22 +7,27 @@ const LoginPage = () => {
     <div className="login-container">
     <h2>Login to Let&apos;s Assist</h2>
       <div className="oauth-buttons">      
-        <button className="oauth-button github">Sign in with GitHub
-        <img src="/github-logo.png" className="github-icon" alt="Google sign-in"></img>
-        </button>
-        <button className="oauth-button google">Sign in with Google
-        <img src="/google-logo.png" className="google-icon" alt="Google sign-in"></img>
-        </button>
-        <button className="oauth-button apple">Sign in with Apple
-        <img src="/apple-logo.png" className="apple-icon" alt="Apple sign-in"></img>
-        </button>
+      <a href="https://github.com/login/oauth/authorize" className="oauth-button github">
+    Sign in with GitHub
+    <img src="/github-logo.png" className="github-icon" alt="GitHub sign-in" />
+</a>
+<a href="https://accounts.google.com/o/oauth2/v2/auth" className="oauth-button google">
+    Sign in with Google
+    <img src="/google-logo.png" className="google-icon" alt="Google sign-in" />
+</a>
+<a href="https://appleid.apple.com/auth/authorize" className="oauth-button apple">
+    Sign in with Apple
+    <img src="/apple-logo.png" className="apple-icon" alt="Apple sign-in" />
+</a>
       </div>
       <form className="login-form">
         <input type="text" placeholder="Username" className="input-field" />
         <input type="password" placeholder="Password" className="input-field" />
         <button type="submit" className="login-button">Login</button>
       </form>
-      <p>Don&apos;t have an account? <a href="/signup">Sign up here</a></p>
+      <div className="signup-link">
+        <p>Don&apos;t have an account? <a href="/signup">Sign up here</a></p>
+    </div>
     </div>
     </>
   );
