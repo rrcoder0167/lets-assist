a9235657f3b51eac78967f53ca1ec175d11d68f7
rrcoder0167 <riddhiman.rana@gmail.com>
2023-11-05 16:43:52 -0800

    unfixing and refixing stuff

--- next.config.js
+++ next.config.js
@@ -1,17 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'placehold.co'
-        },
-        {
-            protocol: 'https',
-            hostname: 'avatars.githubusercontent.com'
-        }
-      ],
-    },
-  }
+module.exports = nextConfig
