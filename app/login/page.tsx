0acd22529664a0b3c1b1e4540dcc8ae299f4efc7
rrcoder0167 <riddhiman.rana@gmail.com>
2023-11-06 19:54:04 -0800

    Add catppuccin-frappe.css and update email input type.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -23,7 +23,7 @@ const LoginPage = () => {
           </a>
         </div>
         <form className="login-form">
-          <input type="text" placeholder="Email" className="input-field" />
+          <input type="email" placeholder="Email" className="input-field" />
           <input type="password" placeholder="Password" className="input-field" />
           <button type="submit" className="login-button">Login</button>
         </form>
