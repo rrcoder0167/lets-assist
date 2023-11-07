5f3ba78b60ef8039d056acf90c08eb0c93ff755d
rrcoder0167 <riddhiman.rana@gmail.com>
2023-11-06 20:30:39 -0800

    Add sign out button and dashboard component, use next-auth/react for session management again these will be broken, till finished

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,20 @@
+"use client";
 import React from 'react';
 import LandingPage from '@/components/LandingPage';
 import Navbar from '@/components/navbar';
+import Dashboard from '@/components/Dashboard'
+import { useSession } from "next-auth/react";
 
 export default function Home() {
-  return (
-    <>
+  const { status, data: session } = useSession();
+    if (status === "authenticated") {
+      return <>
+      <Dashboard/>
+      </>;
+    } else {
+      return <>
       <Navbar />
       <LandingPage />
-    </>
-  );
-}
+      </>;
+    }
+  }
