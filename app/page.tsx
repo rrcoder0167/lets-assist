7278098dc1870a65176fd1413cfff04456c30f47
rrcoder0167 <riddhiman.rana@gmail.com>
2023-11-06 20:37:33 -0800

    Add Next.js Image component to display user profile picture

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,10 @@ import LandingPage from '@/components/LandingPage';
 import Navbar from '@/components/navbar';
 import Dashboard from '@/components/Dashboard'
 import { useSession } from "next-auth/react";
+import Image from "next/image";
 
 export default function Home() {
-  const { status, data: session } = useSession();
+    const { status, data: session } = useSession();
     if (status === "authenticated") {
       return <>
       <Dashboard/>
