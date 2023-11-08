80bbcb94196236ec9a2743425a4dca74c88d2e0a
Keshav Verma <vkeshav300@gmail.com>
2023-11-07 19:15:50 -0800

    Add login required component

---
+++ components/loginRequired.tsx
@@ -0,0 +1,25 @@
+import React, { ReactNode } from 'react';
+import { useEffect } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
+
+interface LoginRequiredProps {
+    children: ReactNode;
+}
+
+const LoginRequired: React.FC<LoginRequiredProps> = ({ children }) => {
+    const { data: session, status } = useSession();
+    const router = useRouter();
+
+    useEffect(() => {
+        if (status === 'loading') return;
+
+        if (!session) {
+            router.push('/login');
+        }
+    }, [session, status, router]);
+
+    return <>{children}</>;
+};
+
+export default LoginRequired;
