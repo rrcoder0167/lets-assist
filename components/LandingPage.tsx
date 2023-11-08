bd74e387827134dffc619d89bbfdd5bc6e0eab0d
Keshav Verma <vkeshav300@gmail.com>
2023-11-07 18:11:44 -0800

    Remove sign in with google button from homepage

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -7,7 +7,6 @@ import SignInButtons from './signInButtons';
 export default function LandingPage() {
   return (
     <>
-    <SignInButtons />
       <main className="d-flex align-items-center justify-content-center flex-column" style={{ height: '80vh' }}>
         <h1 className="landing-text">Give back to your<br />community, your way</h1>
         <p className="sub-text">Find local volunteering opportunities <span className="highlight">today</span> with Let&apos;s Assist, and give back to your community</p>
