5c28a04a7803fb61e5bf32054fe53c85ddeaf798
rrcoder0167 <riddhiman.rana@gmail.com>
2023-11-08 19:26:53 -0800

    basic user auth functionality

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,11 +1,15 @@
-import { NextResponse } from "next/server"
+import { connectMongoDB } from "@/lib/mongodb";
+import User from "@/models/user";
+import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
 
 export async function POST(req:any) {
     try {
-        const {username, email, password} = await req.json();
-        console.log("Name: ", username);
-        console.log("Email: ", email);
-        console.log("Password: ", password);
+        const {name, email, password} = await req.json();
+        const hashedPassword = await bcrypt.hash(password, 10);
+        await connectMongoDB();
+        await User.create({ name, email, password:hashedPassword });
+
         return NextResponse.json({message: "User registered."}, {status: 201});
     } catch (error) {
         return NextResponse.json(
