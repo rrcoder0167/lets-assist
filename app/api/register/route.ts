a64919791cefa51a6eeba7bc60b39d69afd226e8
rrcoder0167 <riddhiman.rana@gmail.com>
2023-11-08 23:03:39 -0800

    90& fixed commit

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -4,16 +4,17 @@ import { NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
 
 export async function POST(req:any) {
-    try {
-        const {name, email, password} = await req.json();
-        const hashedPassword = await bcrypt.hash(password, 10);
-        await connectMongoDB();
-        await User.create({ name, email, password:hashedPassword });
+  try {
+    const { name, email, password } = await req.json();
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await connectMongoDB();
+    await User.create({ name, email, password: hashedPassword });
 
-        return NextResponse.json({message: "User registered."}, {status: 201});
-    } catch (error) {
-        return NextResponse.json(
-            { message: "An error occurred while registering the user."}, { status: 500 }
-        )
-    }
+    return NextResponse.json({ message: "User registered." }, { status: 201 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "An error occurred while registering the user." },
+      { status: 500 }
+    );
+  }
 }
--- app/api/userExists/route.ts
+++ app/api/userExists/route.ts
@@ -2,14 +2,14 @@ import { connectMongoDB } from "@/lib/mongodb";
 import User from "@/models/user";
 import { NextResponse } from "next/server";
 
-export async function POST(req){
-    try {
-        await connectMongoDB();
-        const { email } = await req.json();
-        const user = await User.findOne({email}).select("_id");
-        console.log(user)
-        return NextResponse.json({ user });
-    } catch(error) {
-        console.log(error);
-    }
+export async function POST(req) {
+  try {
+    await connectMongoDB();
+    const { email } = await req.json();
+    const user = await User.findOne({ email }).select("_id");
+    console.log("user: ", user);
+    return NextResponse.json({ user });
+  } catch (error) {
+    console.log(error);
+  }
 }
