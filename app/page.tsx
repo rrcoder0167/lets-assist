1fdf9555e00d3f5f80aadd3d494a3af3d6e61811
rrcoder0167 <riddhiman.rana@gmail.com>
2023-11-12 16:10:46 +0530

    vercel fixes try 3

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from 'react';
 import LandingPage from '@/components/LandingPage';
-import Dashboard from "@/components/Dashboard";
+import Dashboard from '@/components/Dashboard';
 import { useSession } from "next-auth/react";
 
 export default function Home() {
