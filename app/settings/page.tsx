847d3e5d3c4b9eacf3f6203c5e2b8dc2ef8ce545
rrcoder0167 <riddhiman.rana@gmail.com>
2023-11-12 16:55:57 +0530

    add gitignore for codesandbox, settings route

---
+++ app/settings/page.tsx
@@ -0,0 +1,22 @@
+export default function LoginPage() {
+    return (
+        <div className="container">
+            <h1>Settings</h1>
+            <form>
+                <div className="mb-3">
+                    <label htmlFor="username" className="form-label">Username</label>
+                    <input type="text" className="form-control" id="username" />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label">Email address</label>
+                    <input type="email" className="form-control" id="email" />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <input type="password" className="form-control" id="password" />
+                </div>
+                <button type="submit" className="btn btn-primary">Save Changes</button>
+            </form>
+        </div>
+    )
+};
