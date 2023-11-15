318f42cfbf5dc2cc26f9621364fc916c74c08522
rrcoder0167 <riddhiman.rana@gmail.com>
2023-11-15 19:12:44 +0530

    commented out api code for categories

--- components/CategoriesList.tsx
+++ components/CategoriesList.tsx
@@ -18,3 +18,42 @@ export default function CategoriesList() {
     </div>
     );
 }
+
+/* need to fix this later
+import Link from "next/link";
+import "./CategoriesList.css";
+import { TCategory } from "@/app/types";
+
+const getCategories = async (): Promise<TCategory[] | null> => {
+    try {
+        const res = await fetch(`${process.env.NEXTAUTH_URL}/api/categories`);
+
+        if (res.ok) {
+            const categories = await res.json();
+            return categories;
+        }
+    } catch(error) {
+        console.log(error);
+        return null;
+    }
+    return null;
+}
+
+export default async function CategoriesList() {
+    const categories = await getCategories();
+    return (
+    <div className="categories-section">
+    {categories &&
+        categories.map((category) => (
+        <Link
+            key={category.id}
+            className="category"
+            href={`/categories/${category.catName}`}
+        >
+            {category.catName}
+        </Link>
+        ))}
+    </div>
+    );
+}
+*
