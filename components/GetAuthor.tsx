21b708481ecddb30d5129847c82722415a5d7043
Keshav Verma <vkeshav300@gmail.com>
2023-11-23 09:45:33 -0800

    Fix learn-more page

---
+++ components/GetAuthor.tsx
@@ -0,0 +1,18 @@
+import { TProject } from "@/app/types";
+
+export const GetAuthor = async (email: string): Promise<TProject | null> => {
+    try {
+        const res = await fetch(`http://localhost:3000/api/authors/${email}`, {
+            cache: "no-store",
+        });
+
+        if (res.ok) {
+            const post = await res.json();
+            return post;
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+    return null;
+};
