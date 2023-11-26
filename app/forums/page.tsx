e49f55dbc712d157440b090e8777c2ee5ba2256b
rrcoder0167 <riddhiman.rana@gmail.com>
2023-11-25 20:24:22 -0800

    minor changes to files

--- app/forums/page.tsx
+++ app/forums/page.tsx
@@ -1,10 +1,17 @@
 import "./page.css";
+import { useRouter } from "next/navigation";
 
 export default function Forums()
 {
+    const router = useRouter();
+    const handleRefresh = () => {
+        router.refresh();
+    };
     return (
         <>
-            <h1>Ye Forum Stuff Bro</h1>
+            <h1>Community Forums</h1>
+            <h3>Have a question? Ask it here!</h3>
+            <button onClick={handleRefresh}>Refresh this page</button>
         </>
     );
 }
