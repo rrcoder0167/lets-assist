c7838ddf15d332157d84ff8d7b3a7cebf662a63d
Keshav Verma <vkeshav300@gmail.com>
2023-11-26 16:03:49 -0800

    Attempt to fix credentials provider

--- app/learn-more/[id]/page.tsx
+++ app/learn-more/[id]/page.tsx
@@ -26,7 +26,7 @@ export default async function LearnMore({ params }: { params: { id: string } }) 
         <>
             <title>{project?.title}</title>
             <h3><p className="text-muted d-inline">Learn More About</p> {project?.title}</h3>
-            <div className="text-response">
+            <div className="text-cst-response">
                 <p className="text-muted d-inline">Description <FaReceipt /></p>
                 <p>{project?.description}</p>
                 <p className="text-muted d-inline">Location <FaMapMarkerAlt /></p>
