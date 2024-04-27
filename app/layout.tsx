225b5639107ccfc71a41fbd5e469452cc48f9fe5
Keshav Verma <vkeshav300@gmail.com>
2024-04-27 11:05:17 -0700

    Implement navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,12 @@ import * as React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import { NextUIProvider } from "@nextui-org/react";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, NextUIProvider } from "@nextui-org/react";
+import { Button as ButtonSCN, buttonVariants } from "@/components/ui/button";
+import LetsAssistLogo from "@/components/logos/letsAssist";
 import "./globals.css";
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator } from "@/components/ui/dropdown-menu";
+import { Contact, Proportions, SquareUser, Telescope, UserCog } from "lucide-react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +25,44 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <NextUIProvider>
+          {/* Navbar */}
+          <Navbar shouldHideOnScroll className="bg-primary">
+            <NavbarBrand>
+              <div className="w-7"><LetsAssistLogo /></div>
+              <ButtonSCN>Let's Assist</ButtonSCN>
+            </NavbarBrand>
+            <NavbarContent justify="center">
+              <NavbarItem>
+                <ButtonSCN variant="default"><Proportions className="w-5 pr-1" /> Dashboard</ButtonSCN>
+              </NavbarItem>
+              <NavbarItem>
+                <ButtonSCN><Telescope className="w-5 pr-1" />Discover</ButtonSCN>
+              </NavbarItem>
+            </NavbarContent>
+            <NavbarContent justify="end">
+              <NavbarItem>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <ButtonSCN><SquareUser className="w-5" /></ButtonSCN>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent className="w-25">
+                    <DropdownMenuLabel>My Stuff</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuGroup>
+                      <DropdownMenuItem>
+                        <UserCog className="w-5 pr-1" />
+                        <span>Account</span>
+                      </DropdownMenuItem>
+                      <DropdownMenuItem>
+                        <Contact className="w-5 pr-1" />
+                        <span>Profile</span>
+                      </DropdownMenuItem>
+                    </DropdownMenuGroup>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </NavbarItem>
+            </NavbarContent>
+          </Navbar>
           {children}
         </NextUIProvider>
         <SpeedInsights />
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import { Button } from "@nextui-org/react";
-import { Button as Button2 } from "@/components/ui/button";
+import { Button as ButtonSCN } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="">
-        <Button className="mr-4" color="primary">NextUI Sign Up Button</Button>
-        <Button2>ShadCN Button</Button2>
+        <Button className="m-2" color="primary">NextUI Sign Up Button</Button>
+        <ButtonSCN className="m-2 rounded-xl">ShadCN Button</ButtonSCN>
       </div>
     </div>
   );
