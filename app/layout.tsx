8f1feb368bd36fd1fe23136f0d88b12c5255d2d1
rrcoder0167 <rrcoder0167@users.noreply.github.com>
2024-04-30 14:17:08 -0700

    clean up code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,10 @@
 import * as React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Link, Navbar, NavbarBrand, NavbarContent, NavbarItem, NextUIProvider } from "@nextui-org/react";
-import { Button as ButtonSCN, buttonVariants } from "@/components/ui/button";
-import LetsAssistLogo from "@/components/logos/letsAssist";
+import { NextUIProvider } from "@nextui-org/react";
 import "./globals.css";
-import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator } from "@/components/ui/dropdown-menu";
-import { Contact, Proportions, SquareUser, Telescope, UserCog } from "lucide-react";
-import { redirect } from "next/navigation";
+import Navbar from "@/components/Navbar";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,48 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <NextUIProvider>
-          {/* Navbar */}
-          <Navbar shouldHideOnScroll className="bg-primary">
-            <NavbarBrand>
-              <ButtonSCN asChild>
-                <Link href="/home">
-                  <span className="w-7"><LetsAssistLogo /></span>
-                  <span className="pl-3">Let&apos;s Assist</span>
-                </Link>
-              </ButtonSCN>
-            </NavbarBrand>
-            <NavbarContent justify="center">
-              <NavbarItem>
-                <ButtonSCN variant="default"><Proportions className="w-5 pr-1" /> Dashboard</ButtonSCN>
-              </NavbarItem>
-              <NavbarItem>
-                <ButtonSCN><Telescope className="w-5 pr-1" />Discover</ButtonSCN>
-              </NavbarItem>
-            </NavbarContent>
-            <NavbarContent justify="end">
-              <NavbarItem>
-                <DropdownMenu>
-                  <DropdownMenuTrigger asChild>
-                    <ButtonSCN><SquareUser className="w-5" /></ButtonSCN>
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent className="w-25">
-                    <DropdownMenuLabel>My Stuff</DropdownMenuLabel>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuGroup>
-                      <DropdownMenuItem>
-                        <UserCog className="w-5 pr-1" />
-                        <span>Account</span>
-                      </DropdownMenuItem>
-                      <DropdownMenuItem>
-                        <Contact className="w-5 pr-1" />
-                        <span>Profile</span>
-                      </DropdownMenuItem>
-                    </DropdownMenuGroup>
-                  </DropdownMenuContent>
-                </DropdownMenu>
-              </NavbarItem>
-            </NavbarContent>
-          </Navbar>
+          <Navbar />
           <div className="bg-secondary">
             {children}
           </div>
---
+++ components/Navbar.tsx
@@ -0,0 +1,51 @@
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator } from "@/components/ui/dropdown-menu";
+import { Contact, Proportions, SquareUser, Telescope, UserCog } from "lucide-react";
+import { Button as ButtonSCN, buttonVariants } from "@/components/ui/button";
+import LetsAssistLogo from "@/components/logos/letsAssist";
+import { Link, Navbar as NextNavbar, NavbarBrand, NavbarContent, NavbarItem} from "@nextui-org/react";
+
+export default function Navbar() {
+    return (
+        <NextNavbar shouldHideOnScroll className="bg-primary">
+            <NavbarBrand>
+              <ButtonSCN asChild>
+                <Link href="/home">
+                  <span className="w-7"><LetsAssistLogo /></span>
+                  <span className="pl-3">Let&apos;s Assist</span>
+                </Link>
+              </ButtonSCN>
+            </NavbarBrand>
+            <NavbarContent justify="center">
+              <NavbarItem>
+                <ButtonSCN variant="default"><Proportions className="w-5 pr-1" /> Dashboard</ButtonSCN>
+              </NavbarItem>
+              <NavbarItem>
+                <ButtonSCN><Telescope className="w-5 pr-1" />Discover</ButtonSCN>
+              </NavbarItem>
+            </NavbarContent>
+            <NavbarContent justify="end">
+              <NavbarItem>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <ButtonSCN><SquareUser className="w-5" /></ButtonSCN>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent className="w-25">
+                    <DropdownMenuLabel>My Stuff</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuGroup>
+                      <DropdownMenuItem>
+                        <UserCog className="w-5 pr-1" />
+                        <span>Account</span>
+                      </DropdownMenuItem>
+                      <DropdownMenuItem>
+                        <Contact className="w-5 pr-1" />
+                        <span>Profile</span>
+                      </DropdownMenuItem>
+                    </DropdownMenuGroup>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </NavbarItem>
+            </NavbarContent>
+          </NextNavbar>
+    )
+}
