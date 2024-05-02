96bce436bfbc33c9168dadaaf205273cf9a86aa5
rrcoder0167 <rrcoder0167@users.noreply.github.com>
2024-05-01 23:25:31 -0700

    updated navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,21 +3,10 @@ import React from "react";
 import LetsAssistLogo from "@/components/letsAssistLogo";
 import { Navbar as NextNavbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Link, Button } from "@nextui-org/react";
 import { Separator } from "@/components/ui/separator";
+import { LayoutDashboard } from "lucide-react";
 
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-  const menuItems = [
-    "Profile",
-    "Dashboard",
-    "Activity",
-    "Analytics",
-    "System",
-    "Deployments",
-    "My Settings",
-    "Team Settings",
-    "Help & Feedback",
-    "Log Out",
-  ];
   return (
     <>
     <NextNavbar onMenuOpenChange={setIsMenuOpen} shouldHideOnScroll>
@@ -33,47 +22,47 @@ export default function Navbar() {
     </NavbarContent>
 
     <NavbarContent className="hidden sm:flex gap-4" justify="center">
+      <NavbarItem isActive>
+        <Link color="primary" href="/">
+          Home
+        </Link>
+      </NavbarItem>
       <NavbarItem>
-        <Link color="foreground" href="#">
+        <Link color="foreground" href="#features">
           Features
         </Link>
       </NavbarItem>
-      <NavbarItem isActive>
-        <Link href="#" aria-current="page">
-          Customers
-        </Link>
-      </NavbarItem>
       <NavbarItem>
-        <Link color="foreground" href="#">
-          Integrations
+        <Link color="foreground" href="#pricing">
+          Contact Us
         </Link>
       </NavbarItem>
     </NavbarContent>
+
     <NavbarContent justify="end">
-      <NavbarItem className="hidden lg:flex">
-        <Link href="#">Login</Link>
-      </NavbarItem>
       <NavbarItem>
         <Button as={Link} color="primary" href="#" variant="flat">
-          Sign Up
+          Join the Waitlist
         </Button>
       </NavbarItem>
     </NavbarContent>
+
     <NavbarMenu>
-      {menuItems.map((item, index) => (
-        <NavbarMenuItem key={`${item}-${index}`}>
-          <Link
-            color={
-              index === 2 ? "primary" : index === menuItems.length - 1 ? "danger" : "foreground"
-            }
-            className="w-full"
-            href="#"
-            size="lg"
-          >
-            {item}
-          </Link>
-        </NavbarMenuItem>
-      ))}
+      <NavbarMenuItem>
+        <Link color="foreground" className="w-full" href="#" size="lg">
+          Home
+        </Link>
+      </NavbarMenuItem>
+      <NavbarMenuItem>
+        <Link color="foreground" className="w-full" href="#" size="lg">
+          Features
+        </Link>
+      </NavbarMenuItem>
+      <NavbarMenuItem>
+        <Link color="primary" className="w-full" href="#" size="lg">
+          Pricing
+        </Link>
+      </NavbarMenuItem>
     </NavbarMenu>
   </NextNavbar>
   <Separator />
