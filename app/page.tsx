836367850957dfa529de869a32dd89977adb3505
Keshav Verma <vkeshav300@gmail.com>
2024-05-02 18:19:57 -0700

    Update home page features section

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { MailPlus, OctagonAlert, UserPlus } from "lucide-react";
 import Image from "next/image";
 import PlaceholderImage from "@/public/placeholder-500-300.svg";
 import PrototypeDashboard from "@/public/prototype-dashboard.png";
+import PrototypeManageProject from "@/public/prototype-manage-project.png";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog";
 import { Input } from "@/components/ui/input";
@@ -26,7 +27,7 @@ export default function Home() {
         <p className="text-lg pb-5">Find local volunteering opportunities today with Let&apos;s Assist, and give back to your community.</p>
         <Alert variant="default" className="my-10">
           <AlertTitle>Note From Developers</AlertTitle>
-          <AlertDescription>Let&apos;s Assist is still in development! Everything you see is a work in progress and may not even function! If you want to receive updates on development join the waitlist by clicking the button below. This lets us know how many people are intrested in this software, as well as lets you know what major development changes have happened.</AlertDescription>
+          <AlertDescription>Let&apos;s Assist is still in development! Everything you see is a work in progress, is not final, and may not even function! If you want to receive updates on development, join the waitlist by clicking the button below. This also lets us know how many people are intrested in this software.</AlertDescription>
         </Alert>
         {/* <ButtonSCN variant="default">
           <UserPlus className="pr-1" />
@@ -47,7 +48,7 @@ export default function Home() {
               </AlertDialogDescription>
             </AlertDialogHeader>
             <AlertDialogFooter>
-              <Input type="email" placeholder="Enter Your Email Here" />
+              <Input type="email" placeholder="enter@email.here" />
               <AlertDialogAction className="bg-success-500 hover:bg-success-700">Sign Me Up!</AlertDialogAction>
               <AlertDialogCancel>Nevermind</AlertDialogCancel>
             </AlertDialogFooter>
@@ -58,16 +59,16 @@ export default function Home() {
       <div>
         <h1 className="text-3xl flex justify-center m-5 font-bold" id="features">Features</h1>
         <div className="grid grid-cols-2 justify-center m-5 p-1 space-y-10">
-          <Image src={PrototypeDashboard} alt="A demo user-interface for the Let&apos;s Assist dashboard page" width={500} height={300} className="rounded-md" />
+          <Image src={PrototypeDashboard} alt="A demo user-interface for the dashboard page" width={500} height={300} className="rounded-md" />
           <div>
             <h1 className="text-xl font-bold">Find Volunteering Opportunities</h1>
             <p className="text-sm">Finding volunteering opportunities has never been easier than using Let&apos;s Assist&apos;s easy-to-use dashboard! Use tags and location matching to sort through opportunities and retrieve the ones that fit you.</p>
           </div>
-          <div>
+          <div className="mr-5">
             <h1 className="text-xl font-bold">Manage Volunteering Opportunities</h1>
-            <p className="text-sm">The event manager page allows you update your event information and post updates about these events.</p>
+            <p className="text-sm">The event manager page allows you update your event information and post updates about these events. You can update project information, download participant sheets, and post updates for participants to see.</p>
           </div>
-          <Image src={PlaceholderImage} alt="Placeholder" width={500} height={300} className="rounded-md" />
+          <Image src={PrototypeManageProject} alt="A demo user-interface for the manage project page" width={500} height={300} className="rounded-md" />
         </div>
       </div>
     </>
