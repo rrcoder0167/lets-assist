8edb97a4f84fd45fcd6db06d76980404bc4a9332
Keshav Verma <vkeshav300@gmail.com>
2024-09-12 17:29:39 -0700

    Update login page

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -10,24 +10,24 @@ import "../globals.css"
 import { KeyRound } from "lucide-react"
 
 const formSchema = z.object({
-  first_name: z.string().min(2, {
-    message: "Minimum field length not achieved"
-  }).max(100, {
+  first_name: z.string().max(100, {
     message: "Maximum field length achieved"
-  }),
-  last_name: z.string().min(2, {
-    message: "Minimum field length not achieved"
-  }).max(100, {
+  }).trim(),
+  last_name: z.string().max(100, {
     message: "Maximum field length achieved"
-  })
-})
+  }).trim(),
+  email: z.string().email({
+    message: "Invalid email"
+  }).trim()
+}).required()
 
-export default function Signup() {
+export default function Register() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       first_name: "",
-      last_name: ""
+      last_name: "",
+      email: ""
     }
   })
 
@@ -36,10 +36,20 @@ export default function Signup() {
     console.log(values)
   }
 
+  function googleProvider() {
+
+  }
+
+  function appleProvider() {
+
+  }
+
   return (
     <>
-      <div className="modals">
-        <Button> <KeyRound className="mr-2" /> Log in with Google </Button>
+      <title>Register</title>
+      <div className="flex flex-col justify-center mx-96 gap-y-3 my-5">
+        <Button onClick={googleProvider} variant="secondary"> <KeyRound className="mr-2" /> Register With Google </Button>
+        <Button onClick={appleProvider}> <KeyRound className="mr-2" /> Register With Apple </Button>
       </div>
       <Form {...form} >
         <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col justify-center mx-96 gap-y-5">
@@ -50,7 +60,7 @@ export default function Signup() {
               <FormItem>
                 <FormLabel><b>First Name</b></FormLabel>
                 <FormControl>
-                  <Input placeholder="Enter your first name here" {...field} />
+                  <Input placeholder="Bob" {...field} />
                 </FormControl>
                 <FormDescription>
                   Use your real name.
@@ -66,7 +76,7 @@ export default function Signup() {
               <FormItem>
                 <FormLabel><b>Last Name / Initial</b></FormLabel>
                 <FormControl>
-                  <Input placeholder="Enter your last name or last initial here" {...field} />
+                  <Input placeholder="B" {...field} />
                 </FormControl>
                 <FormDescription>
                   If you don{"'"}t want to use your full last name, you can use your last initial.
@@ -75,7 +85,20 @@ export default function Signup() {
               </FormItem>
             )}
           />
-          <Button type="submit">Create Account</Button>
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel><b>Email</b></FormLabel>
+                <FormControl>
+                  <Input placeholder="bob@example.com" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" variant="constructive">Create Account</Button>
         </form>
       </Form>
     </>
