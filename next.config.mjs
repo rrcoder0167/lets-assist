e2f689ae6cac2b6df6749b49b04492e13b21925f
rrcoder0167 <rrcoder0167@users.noreply.github.com>
2024-09-14 12:32:54 -0700

    Remove unused files and update dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        dangerouslyAllowSVG: true,
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "placehold.co",
-                port: "",
-                pathname: "/500x300"
-            }
-        ]
-    }
-};
+const nextConfig = {};
 
 export default nextConfig;
