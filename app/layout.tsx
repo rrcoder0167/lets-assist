6acad1a4ef6877149c84db876f4e38532fed02a0
rrcoder0167 <rrcoder0167@users.noreply.github.com>
2024-09-14 12:49:51 -0700

    add vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import Navbar from "@/components/navbar"
 
 const inter = Inter({ subsets: ["latin"] });
@@ -27,6 +28,7 @@ export default function RootLayout({
           >
           <Navbar />
         {children}
+        <SpeedInsights />
         </ThemeProvider>
       </body>
     </html>
