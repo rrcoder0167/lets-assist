82bfc5a083e58078e005707a3d4c1eddf48d51e2
rrcoder0167 <rrcoder0167@users.noreply.github.com>
2024-10-20 12:34:40 -0700

    Refactor landing page layout and add HeroSection component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
+import { HeroSection } from "@/components/Hero";
 
 export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <div className="xl:p-10 md:p-10 sm:p-7 p-5 mb-72">
+      <HeroSection />
+      {/* <div className="xl:p-10 md:p-10 sm:p-7 p-5 mb-72">
         <h1 className="xl:text-8xl md:text-7xl sm:text-6xl text-5xl font-black pb-5">
           Give back to <span className="text-primary">your<br />community, your way</span>
         </h1>
-      </div>
+      </div> */}
     </div>
   );
 }
