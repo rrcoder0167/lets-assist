fb1b19c4affdb529bb626a89702f116a5589af53
rrcoder0167 <rrcoder0167@users.noreply.github.com>
2024-10-20 15:40:08 -0700

    Refactor layout and update theme colors

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link"
-
 import { Button } from "@/components/ui/button"
 import {
     Card,
@@ -11,65 +10,48 @@ import {
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 
+import { Mail } from "lucide-react"
+
 export default function LoginForm() {
     return (
         <div className="flex items-center justify-center min-h-screen">
-            <Card className="mx-auto max-w-sm mb-12">
-                <CardHeader>
-                    <CardTitle className="text-xl">Sign Up</CardTitle>
-                    <CardDescription>
-                        Enter your information to create an account
+            <Card className="w-full max-w-sm mx-auto mb-12">
+                <CardHeader className="space-y-1">
+                    <CardTitle className="text-2xl text-left">Create an account</CardTitle>
+                    <CardDescription className="text-left">
+                        Enter your email below to create your account
                     </CardDescription>
                 </CardHeader>
-                <CardContent>
-                    <div className="grid gap-4">
-                        <div className="grid grid-cols-2 gap-4">
-                            <div className="grid gap-2">
-                                <Label htmlFor="first-name">First name</Label>
-                                <Input id="first-name" placeholder="Kai" required />
-                            </div>
-                            <div className="grid gap-2">
-                                <Label htmlFor="last-name">Last name</Label>
-                                <Input id="last-name" placeholder="Cenat" required />
-                            </div>
+                <CardContent className="space-y-4">
+                    <Button variant="outline" className="w-full">
+                        <svg className="mr-2 h-4 w-4" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512">
+                            <path fill="currentColor" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path>
+                        </svg>
+                        Login with Google
+                    </Button>
+                    <div className="relative">
+                        <div className="absolute inset-0 flex items-center">
+                            <span className="w-full border-t" />
                         </div>
-                        <div className="grid gap-2">
-                            <Label htmlFor="email">Email</Label>
-                            <Input
-                                id="email"
-                                type="email"
-                                placeholder="kaicenat@example.com"
-                                required
-                            />
+                        <div className="relative flex justify-center text-xs uppercase">
+                            <span className="bg-card px-2 text-muted-foreground">
+                                Or continue with
+                            </span>
                         </div>
-                        <div className="grid gap-2">
-                            <Label htmlFor="password">Password</Label>
-                            <Input id="password" type="password" />
-                        </div>
-                        <Button type="submit" className="w-full">
-                            Create an account
-                        </Button>
-                        <Button variant="outline" className="w-full">
-                            <svg
-                                className="mr-2 h-4 w-4"
-                                aria-hidden="true"
-                                focusable="false"
-                                data-prefix="fab"
-                                data-icon="google"
-                                role="img"
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 488 512"
-                            >
-                                <path
-                                    fill="currentColor"
-                                    d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
-                                ></path>
-                            </svg>
-                            Sign up with Google
-                        </Button>
                     </div>
+                    <div className="space-y-2">
+                        <Label htmlFor="email">Email</Label>
+                        <Input id="email" type="email" placeholder="m@example.com" />
+                    </div>
+                    <div className="space-y-2">
+                        <Label htmlFor="password">Password</Label>
+                        <Input id="password" type="password" />
+                    </div>
+                    <Button className="w-full">
+                        <Mail className="mr-2 h-4 w-4" /> Create Account
+                    </Button>
                     <div className="mt-4 text-center text-sm">
-                        Already have an account?{" "}
+                    Already have an account?{" "}
                         <Link href="/login" className="underline">
                             Sign in
                         </Link>
