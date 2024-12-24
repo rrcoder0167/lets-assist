e4db6e41de9323c22f594da77b37f06682b5dc88
rrcoder0167 <rrcoder0167@users.noreply.github.com>
2024-12-24 13:44:47 -0800

    Enhance email confirmation flow and integrate toast notifications; update Next.js version

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,33 @@
+'use client'
 import { HeroSection } from "@/components/Hero";
-import { Metadata } from 'next';
+import { useEffect } from 'react'
+import { useSearchParams } from 'next/navigation'
+import { Toaster, toast } from 'sonner'
+import { Suspense } from 'react'
 
-export const metadata: Metadata = {
-  title: "Lets Assist: The Ultimate Volunteering Platform",
-  description:
-    "The ultimate volunteering platform to help you find volunteering opportunities. Give back to your community, your way.",
-};
+function HomeContent() {
+  const searchParams = useSearchParams()
+  useEffect(() => {
+    const emailConfirmed = searchParams.get('confirmed')
+    if (emailConfirmed === 'true') {
+        toast.success('Success! Email verified. Login to continue.', {
+            duration: 5000
+        })
+    }
+  }, [searchParams])
+
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <Toaster position="bottom-right" theme="dark" richColors />
+      <HeroSection />
+    </div>
+  )
+}
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <HeroSection />
-      {/* <div className="xl:p-10 md:p-10 sm:p-7 p-5 mb-72">
-        <h1 className="xl:text-8xl md:text-7xl sm:text-6xl text-5xl font-black pb-5">
-          Give back to <span className="text-primary">your<br />community, your way</span>
-        </h1>
-      </div> */}
-    </div>
-  );
+    <Suspense fallback={<div>Loading...</div>}>
+      <HomeContent />
+    </Suspense>
+  )
 }
