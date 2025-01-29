35d8c8bb4c4a31d72beb28a9f919e667f58c90ac
rrcoder0167 <rrcoder0167@users.noreply.github.com>
2025-01-28 20:55:19 -0800

    Refactor authentication flow: implement middleware for user session management, update logout functionality, and enhance home page with user greeting and event cards

--- app/logout/actions.ts
+++ app/logout/actions.ts
@@ -1,19 +1,17 @@
 'use server'
-
-import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
-
 import { createClient } from '@/utils/supabase/server'
+import { revalidatePath } from 'next/cache'
 
 export async function logout() {
-    const supabase = await createClient()
+  const supabase = await createClient()
   
-    const { error } = await supabase.auth.signOut()
+  const { error } = await supabase.auth.signOut()
   
-    if (error) {
-      redirect('/error')
-    }
-  
+  if (error) {
+    redirect('/error')
+  } else {
     revalidatePath('/', 'layout')
     redirect('/')
   }
+}
