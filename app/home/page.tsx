b3a0e6aa95757280f1e983c4307a9901cae6e8e4
rrcoder0167 <rrcoder0167@users.noreply.github.com>
2025-02-18 13:18:02 -0800

    Remove unused alert component and related imports from Home page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
 import { createClient } from '@/utils/supabase/server'
 // import DemoClientComponent from "@/components/DemoClientComponent";
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
-import { AlertCircle } from "lucide-react"
-import CompleteProfileButton from "@/components/CompleteProfileButton"
 import { EmailVerificationToast } from "@/components/EmailVerificationToast";
 
 export default async function Home(){
@@ -37,20 +34,6 @@ export default async function Home(){
                             {/* Display recent activities or stats */}
                         </CardContent>
                     </Card>
-                </div>
-                <div className="mt-8 mb-8">
-                    <Alert className="flex justify-between items-center">
-                        <div className="flex items-center">
-                            <AlertCircle className="h-4 w-4 mr-2" />
-                            <div>
-                                <AlertTitle>Heads up!</AlertTitle>
-                                <AlertDescription>
-                                    You need to complete your profile to start volunteering.
-                                </AlertDescription>
-                            </div>
-                        </div>
-                        <CompleteProfileButton />
-                    </Alert>
                 </div>
                 {/* <DemoClientComponent /> */}
             </main>
