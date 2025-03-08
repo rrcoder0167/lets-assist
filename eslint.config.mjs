3304aa94e7df2963eb96e923aff601b404b535e7
rrcoder0167 <rrcoder0167@users.noreply.github.com>
2025-03-08 14:13:49 -0800

    Refactor Toaster implementation to use ToasterTheme component for dynamic theming; update ESLint configuration for better rule management

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,17 @@
-import { FlatCompat } from '@eslint/eslintrc'
+import { FlatCompat } from '@eslint/eslintrc';
 
 const compat = new FlatCompat({
-  // import.meta.dirname is available after Node.js v20.11.0
   baseDirectory: import.meta.dirname,
-})
+});
 
 const eslintConfig = [
   ...compat.config({
     extends: ['next', 'prettier'],
     rules: {
       'react-hooks/exhaustive-deps': 'off',
-    }
+      '@next/next/no-duplicate-head': 'off', // Disable the rule
+    },
   }),
-]
+];
 
-export default eslintConfig
+export default eslintConfig;
